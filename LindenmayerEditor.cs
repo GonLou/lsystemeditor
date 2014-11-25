b24158f8a3cf98371e6e1ab93af689dca89a56b2
GonLou <gnunodal@hotmail.com>
2014-11-25 11:37:56 +0000

    record manipulator

--- LindenmayerEditor.cs
+++ LindenmayerEditor.cs
@@ -14,16 +14,23 @@ public class LindenmayerEditor : Editor {
 		//toggleSaveFile = GUI.Toggle(new Rect(10, 10, 100, 30), toggleSaveFile, "save to file");
 
 		if (GUILayout.Button("Generate String")) {
-			//GUIText.text = "wait a moment, this can take a while";
 			myScript.CreateFullAxiom();
 			myScript.Axiom2Vectors();
 			// save to file if toggle is checked
 			if (GUI.Toggle(new Rect(10, 10, 100, 30), myScript.toggleSaveFile, "save to file")) myScript.SaveFile();
-			//GUIText.text = "";
+			myScript.drawEnable = true;
 		}
 
-		if (GUILayout.Button("Load from file")) {
+		if (GUILayout.Button("+")) {
+			myScript.BiggerRecord();
+			myScript.LoadFile(myScript.record_number);
+		}
 
+		EditorGUILayout.LabelField("Record number # ", (myScript.record_number+1).ToString());
+
+		if (GUILayout.Button("-")) {
+			myScript.SmallerRecord();
+			myScript.LoadFile(myScript.record_number);
 		}
 
 		if (EditorGUI.EndChangeCheck()) {
